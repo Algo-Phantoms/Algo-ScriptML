96fcfd5356d80353c0b8b3c523b6deecdd371974
Simarjeet <b.simarjeet123@gmail.com>
2021-03-15 08:48:21 +0530

    implemented random forest with new changes

--- Random Forest/random_forest/random_forest.py
+++ Random Forest/random_forest/random_forest.py
@@ -1,24 +1,85 @@
-import pandas as pd
-import numpy as np
-dataset = pd.read_csv("D:/Datasets/bill_authentication.csv")
-dataset.head()
-X = dataset.iloc[:, 0:4].values
-y = dataset.iloc[:, 4].values
-from sklearn.model_selection import train_test_split
+class RandomForest():
+    def __init__(self, x, y, n_trees, n_features, sample_sz, depth=10, min_leaf=5):
+        np.random.seed(12)
+        if n_features == 'sqrt':
+            self.n_features = int(np.sqrt(x.shape[1]))
+        elif n_features == 'log2':
+            self.n_features = int(np.log2(x.shape[1]))
+        else:
+            self.n_features = n_features
+        print(self.n_features, "sha: ",x.shape[1])    
+        self.x, self.y, self.sample_sz, self.depth, self.min_leaf  = x, y, sample_sz, depth, min_leaf
+        self.trees = [self.create_tree() for i in range(n_trees)]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-from sklearn.preprocessing import StandardScaler
+    def create_tree(self):
+        idxs = np.random.permutation(len(self.y))[:self.sample_sz]
+        f_idxs = np.random.permutation(self.x.shape[1])[:self.n_features]
+        return DecisionTree(self.x.iloc[idxs], self.y[idxs], self.n_features, f_idxs,
+                    idxs=np.array(range(self.sample_sz)),depth = self.depth, min_leaf=self.min_leaf)
+        
+    def predict(self, x):
+        return np.mean([t.predict(x) for t in self.trees], axis=0)
 
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
-from sklearn.ensemble import RandomForestRegressor
+def std_agg(cnt, s1, s2): return math.sqrt((s2/cnt) - (s1/cnt)**2)
 
-regressor = RandomForestRegressor(n_estimators=20, random_state=0)
-regressor.fit(X_train, y_train)
-y_pred = regressor.predict(X_test)
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+class DecisionTree():
+    def __init__(self, x, y, n_features, f_idxs,idxs,depth=10, min_leaf=5):
+        self.x, self.y, self.idxs, self.min_leaf, self.f_idxs = x, y, idxs, min_leaf, f_idxs
+        self.depth = depth
+        print(f_idxs)
+#         print(self.depth)
+        self.n_features = n_features
+        self.n, self.c = len(idxs), x.shape[1]
+        self.val = np.mean(y[idxs])
+        self.score = float('inf')
+        self.find_varsplit()
+        
+    def find_varsplit(self):
+        for i in self.f_idxs: self.find_better_split(i)
+        if self.is_leaf: return
+        x = self.split_col
+        lhs = np.nonzero(x<=self.split)[0]
+        rhs = np.nonzero(x>self.split)[0]
+        lf_idxs = np.random.permutation(self.x.shape[1])[:self.n_features]
+        rf_idxs = np.random.permutation(self.x.shape[1])[:self.n_features]
+        self.lhs = DecisionTree(self.x, self.y, self.n_features, lf_idxs, self.idxs[lhs], depth=self.depth-1, min_leaf=self.min_leaf)
+        self.rhs = DecisionTree(self.x, self.y, self.n_features, rf_idxs, self.idxs[rhs], depth=self.depth-1, min_leaf=self.min_leaf)
 
-print(confusion_matrix(y_test,y_pred))
-print(classification_report(y_test,y_pred))
-print(accuracy_score(y_test, y_pred))
+    def find_better_split(self, var_idx):
+        x, y = self.x.values[self.idxs,var_idx], self.y[self.idxs]
+        sort_idx = np.argsort(x)
+        sort_y,sort_x = y[sort_idx], x[sort_idx]
+        rhs_cnt,rhs_sum,rhs_sum2 = self.n, sort_y.sum(), (sort_y**2).sum()
+        lhs_cnt,lhs_sum,lhs_sum2 = 0,0.,0.
+
+        for i in range(0,self.n-self.min_leaf-1):
+            xi,yi = sort_x[i],sort_y[i]
+            lhs_cnt += 1; rhs_cnt -= 1
+            lhs_sum += yi; rhs_sum -= yi
+            lhs_sum2 += yi**2; rhs_sum2 -= yi**2
+            if i<self.min_leaf or xi==sort_x[i+1]:
+                continue
+
+            lhs_std = std_agg(lhs_cnt, lhs_sum, lhs_sum2)
+            rhs_std = std_agg(rhs_cnt, rhs_sum, rhs_sum2)
+            curr_score = lhs_std*lhs_cnt + rhs_std*rhs_cnt
+            if curr_score<self.score: 
+                self.var_idx,self.score,self.split = var_idx,curr_score,xi
+
+    @property
+    def split_name(self): return self.x.columns[self.var_idx]
+    
+    @property
+    def split_col(self): return self.x.values[self.idxs,self.var_idx]
+
+    @property
+    def is_leaf(self): return self.score == float('inf') or self.depth <= 0 
+    
+
+    def predict(self, x):
+        return np.array([self.predict_row(xi) for xi in x])
+
+    def predict_row(self, xi):
+        if self.is_leaf: return self.val
+        t = self.lhs if xi[self.var_idx]<=self.split else self.rhs
+        return t.predict_row(xi)
