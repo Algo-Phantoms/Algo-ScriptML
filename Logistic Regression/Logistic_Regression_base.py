4a69e579e0ee121335b6a80553bc885c2e7280bd
Akanchha Bhadani <akanchha4bhadani@gmail.com>
2021-03-16 16:20:00 +0530

    added function to get the predicted values on trained model

--- Logistic Regression/Logistic_Regression_base.py
+++ Logistic Regression/Logistic_Regression_base.py
@@ -1,11 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-m = 100 #number of training examples
-n = 2 #number of features 
-
-X = np.zeros(shape = [m,n])
-y = np.zeros(shape = [m,1])
 
 #normalizing the features (using mean and standard deviation)
 def normalize_features(X):
@@ -19,7 +14,7 @@ def hypothetical_function(X, theta):
     ones_array = np.ones(shape=[X.shape[0]])
     X_temp = np.insert(X, 0, ones_array, axis = 1).reshape(X.shape[0],X.shape[1]+1)
     return((1/(1+np.exp(-np.sum(theta.transpose()*X_temp, axis = 1)))).reshape(X.shape[0],1))
-    
+
 #cost function
 def compute_cost(X, y, theta):
     #for y==1
@@ -37,7 +32,7 @@ def gradient_descent(X, y, theta, alpha):
     theta = theta - (alpha*delta)
     return theta
 
-#implementing the logistic regression for testing
+#training the model
 def logistic_regression(X, y, theta, alpha, num_itr):
     cost = []
     for i in range (1,num_itr+1):
@@ -48,7 +43,29 @@ def logistic_regression(X, y, theta, alpha, num_itr):
     plt.show()
     return theta
 
-#testing the algorithm
+#testing on trained model
+def calculate_y_predicted(X, y_actual, theta):
+    y_predicted = hypothetical_function(X, theta)
+    s = 0;
+    for i in range(1, len(y_actual)+1):
+        if(y_predicted[i-1] > 0.5):
+            y_predicted[i-1] = 1
+        else:
+            y_predicted[i-1] = 0
+        if (y_actual[i-1] == y_predicted[i-1]):
+            s = s + 1
+        print(y_actual[i-1], " ", y_predicted[i-1])
+    percent_accuracy = (s/(len(y_actual)))*100
+    print( "Accuracy is:", percent_accuracy)
+
+
+"""#testing the algorithm
+m = 100 #number of training examples
+n = 2 #number of features
+
+X = np.zeros(shape = [m,n])
+y = np.zeros(shape = [m,1])
+
 f = open("dataset1.csv", "r")
 i = 0
 j = 0
@@ -67,8 +84,6 @@ for line in f:
         str+=char
     i = i+1
     j = 0
-#print(X)
-#print(y)
 normalize_features(X)
 plt.plot(X[:,0], X[:,1], 'o')
 plt.show()
@@ -76,11 +91,11 @@ theta = np.ones(shape=[n+1,1])
 print(X.shape)
 print(theta.shape)
 print(compute_cost(X, y, theta))
-theta = logistic_regression(X, y, theta, 0.01, 2500)
+theta = logistic_regression(X[0:60,:], y[0:60], theta, 0.01, 2600)
 
 x_val = np.linspace(-0.5,0.5,100)
 y_val = -(theta[0]+theta[1]*x_val)/theta[2]
-plt.plot(X[:,0],X[:,1],'o')
+plt.plot(X[0:60:,0],X[0:60:,1],'o')
 plt.plot(x_val,y_val)
 plt.show()
-    
+calculate_y_predicted(X[60:101,:],y[60:101], theta)"""
