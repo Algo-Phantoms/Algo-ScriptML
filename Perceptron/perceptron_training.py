50ecfa9b756c72ecbecb8185ebd4166f3fdcf9fd
aakriti1318 <aakritiaggarwal2k@gmail.com>
2021-03-19 13:01:43 +0530

    Perceptron

--- Perceptron/perceptron_training.py
+++ Perceptron/perceptron_training.py
@@ -1,103 +1,43 @@
 # Perceptron
 # Maths behind Perceptron Training
-
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.datasets import make_blobs
-
-# ------- Generating the dataset using make_blobs -------
-X,Y = make_blobs(n_samples=800, centers=2, n_features=2, random_state=2)
-print(X.shape, Y.shape)
-
-plt.style.use("seaborn")
-plt.scatter(X[:,0],X[:,1],c=Y,cmap = plt.cm.Accent)
-plt.show()
-
 # -------- MODEL AND HELPER FUNCTIONS ---------
-
 # Sigmoid function is an activation function (denoted as sigma(z)). The output of the sigma(z) belongs to the range 0 to 1. 
 # 0 means - highly negative input and 1 means - highly positive input 
 # This is useful as an activation function when one is interested in probability mapping rather than precise values of input parameter t.
 
-def sigmoid(z):
-    return (1.0)/(1+np.exp(-z))
+import numpy as np
 
-# -------- IMPLEMENT PERCEPTRON LEARNING ALGORITHMS -------
-# - Learn weights
-# - Reduce the loss
-# - Make the predictions
+class Perceptron:
 
-def predict(X,weights):
-    """ X -> mx(n+1) matrix, w -> (nx1)matrix """
-    z = np.dot(X,weights)
-    predictions = sigmoid(z)
-    return predictions
-def loss(X,Y,weights):
-    """ Binary Cross Entropy """
-    Y_ = predict(X,weights)
-    cost = np.mean(-Y*np.log(Y_) - (1-Y)*np.log(1-Y_))
-    return cost
-def update(X,Y,weights,lr):
-    """ Perform weight updates for 1 epoch """
-    Y_ = predict(X,weights)
-    dw = np.dot(X.T,Y_-Y)
-    m = X.shape[0]
-    weights = weights - lr*dw/(float(m))
-    return weights
-def train(X,Y,lr=0.3,maxEpochs = 1000):
-    ones = np.ones((X.shape[0],1))
-    X = np.hstack((ones,X))
-    weights = np.zeros(X.shape[1])
-    for epoch in range(maxEpochs):
-        weights = update(X,Y,weights,lr)
-        if epoch%10==0:
-            l=loss(X,Y,weights)
-            print("Epoch %d Loss %.4f"%(epoch,l))
-    return weights
+    def __init__(self, learning_rate=0.01, n_iters=500):
+        self.lr = learning_rate
+        self.n_iters = n_iters
+        self.activation_func = self._unit_step_func
+        self.weights = None
+        self.bias = None
 
-# - We can not use Mean Squared Error loss fucntion because it is a non convex function. When we minimize
-# the loss using gradient descent there will be high probabilty of getting stuck at local minima.
-# - So to overcome this difficulty we will use Log Loss
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+        y_ = np.array([1 if i > 0 else 0 for i in y])
+        for _ in range(self.n_iters):
+            for idx, x_i in enumerate(X):
+                linear_output = np.dot(x_i, self.weights) + self.bias
+                y_predicted = self.activation_func(linear_output)
+                update = self.lr * (y_[idx] - y_predicted)
 
-weights = train(X,Y)
+                self.weights += update * x_i
+                self.bias += update
 
-# -------- Activation Function ---------
-# 1. The activation function to be used is a subjective decision taken by the data scientist, 
-# based on the problem statement and the form of the desired results.
-# 2. If the learning process is slow or has vanishing or exploding gradients, 
-# the data scientist may try to change the activation function to see if these problems can be resolved.
+    def predict(self, X):
+        linear_output = np.dot(X, self.weights) + self.bias
+        y_predicted = self.activation_func(linear_output)
+        return y_predicted
 
-# ------- PERCEPTRON IMPLEMENTATION PART II -------
-# - MAKE PREDICTION
-# - VISUALISE DESCISION SURFACE 
-# - LINEAR VS NON LINEAR CLASSIFICATION
-
-def getpredict(X_test,weights,labels = True):
-    if X_test.shape[1]!=weights.shape[0]:
-        ones = np.ones((X_test.shape[0],1))
-        X_test = np.hstack((ones,X_test))
-        
-    probs = predict(X_test,weights)
-    if not labels:
-        return probs
-    else:
-        labels = np.zeros(probs.shape)
-        labels[probs>=0.5] = 1
-        return labels
-
-print(weights)
-
-# Data Visualization
-x1 = np.linspace(-10,2,10)
-print(x1)
-x2 = -(weights[0]+weights[1]*x1)/weights[2]
-print(x2)
-
-plt.scatter(X[:,0],X[:,1],c=Y,cmap = plt.cm.Accent)
-plt.plot(x1,x2,c='red')
-plt.show()
-
-# Accuracy
-y_ = getpredict(X,weights,labels= True)
-training_acc = np.sum(y_==Y)/Y.shape[0]
-print(training_acc)
+    def _unit_step_func(self, x):
+        return np.where(x>=0, 1, 0)
+    
+    def accuracy(y_true, y_pred):
+        accuracy = np.sum(y_true == y_pred) / len(y_true)
+        return accuracy
