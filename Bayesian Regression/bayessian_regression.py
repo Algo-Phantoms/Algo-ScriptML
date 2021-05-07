0b91a84c2e9bc94069ca11cc88501fdeb702a65b
goelesha <54431564+goelesha@users.noreply.github.com>
2021-05-07 16:39:53 +0530

    Update bayessian_regression.py

--- Bayesian Regression/bayessian_regression.py
+++ Bayesian Regression/bayessian_regression.py
@@ -1,74 +1,172 @@
-# Pandas and numpy for data manipulation
-import pandas as pd
+#implementation of Bayesian linear regression
 import numpy as np
+from scipy import stats
 
-# Matplotlib and seaborn for visualization
+class BayesLinReg:
+
+    def __init__(self, n_features, alpha, beta):
+        self.n_features = n_features
+        self.alpha = alpha
+        self.beta = beta
+        self.mean = np.zeros(n_features)
+        self.cov_inv = np.identity(n_features) / alpha
+
+    def learn(self, x, y):
+
+        # Update the inverse covariance matrix (Bishop eq. 3.51)
+        cov_inv = self.cov_inv + self.beta * np.outer(x, x)
+
+        # Update the mean vector (Bishop eq. 3.50)
+        cov = np.linalg.inv(cov_inv)
+        mean = cov @ (self.cov_inv @ self.mean + self.beta * y * x)
+
+        self.cov_inv = cov_inv
+        self.mean = mean
+
+        return self
+
+    def predict(self, x):
+
+        # Obtain the predictive mean (Bishop eq. 3.58)
+        y_pred_mean = x @ self.mean
+
+        # Obtain the predictive variance (Bishop eq. 3.59)
+        w_cov = np.linalg.inv(self.cov_inv)
+        y_pred_var = 1 / self.beta + x @ w_cov @ x.T
+
+        return stats.norm(loc=y_pred_mean, scale=y_pred_var ** .5)
+
+    @property
+    def weights_dist(self):
+        cov = np.linalg.inv(self.cov_inv)
+        return stats.multivariate_normal(mean=self.mean, cov=cov)
+
+#progressive validation to measure the performance of a model
+from sklearn import datasets
+from sklearn import metrics
+
+X, y = datasets.load_boston(return_X_y=True)
+
+model = BayesLinReg(n_features=X.shape[1], alpha=.3, beta=1)
+
+y_pred = np.empty(len(y))
+
+for i, (xi, yi) in enumerate(zip(X, y)):
+    y_pred[i] = model.predict(xi).mean()
+    model.learn(xi, yi)
+
+print(metrics.mean_absolute_error(y, y_pred))
+
+
+
+#In a Bayesian linear regression, the weights follow a distribution that quantifies their uncertainty.
+#steps for producing a visualization of both distributions.
+
+from mpl_toolkits.axes_grid1 import ImageGrid
 import matplotlib.pyplot as plt
-import seaborn as sns
+%matplotlib inline
 
-# Linear Regression to verify implementation
-from sklearn.linear_model import LinearRegression
+np.random.seed(42)
 
-# Scipy for statistics
-import scipy
+# Pick some true parameters that the model has to find
+weights = np.array([-.3, .5])
 
-# PyMC3 for Bayesian Inference
-import pymc3 as pm
+def sample(n):
+    for _ in range(n):
+        x = np.array([1, np.random.uniform(-1, 1)])
+        y = np.dot(weights, x) + np.random.normal(0, .2)
+        yield x, y
 
-exercise = pd.read_csv('exercise.csv')
-calories = pd.read_csv('calories.csv')
-df = pd.merge(exercise, calories, on = 'User_ID')
-df = df[df['Calories'] < 300]
-df = df.reset_index()
-df['Intercept'] = 1
-df.head()
+model = BayesLinReg(n_features=2, alpha=2, beta=25)
 
-# Create the features and response
-X = df.loc[:, ['Intercept', 'Duration']]
-y = df.loc[:, 'Calories']
+# The following 3 variables are just here for plotting purposes
+N = 100
+w = np.linspace(-1, 1, 100)
+W = np.dstack(np.meshgrid(w, w))
 
-with pm.Model() as linear_model:
-    # Intercept
-    intercept = pm.Normal('Intercept', mu = 0, sd = 10)
+n_samples = 5
+fig = plt.figure(figsize=(7 * n_samples, 21))
+grid = ImageGrid(
+    fig, 111,  # similar to subplot(111)
+    nrows_ncols=(n_samples, 3),  # creates a n_samplesx3 grid of axes
+    axes_pad=.5  # pad between axes in inch.
+)
 
-    # Slope
-    slope = pm.Normal('slope', mu = 0, sd = 10)
+# We'll store the features and targets for plotting purposes
+xs = []
+ys = []
 
-    # Standard deviation
-    sigma = pm.HalfNormal('sigma', sd = 10)
+def prettify_ax(ax):
+    ax.set_xlim(-1, 1)
+    ax.set_ylim(-1, 1)
+    ax.set_xlabel('$w_1$')
+    ax.set_ylabel('$w_2$')
+    return ax
 
-    # Estimate of mean
-    mean = intercept + slope * X.loc[:, 'Duration']
+for i, (xi, yi) in enumerate(sample(n_samples)):
 
-    # Observed values
-    Y_obs = pm.Normal('Y_obs', mu = mean, sd = sigma, observed = y.values)
+    pred_dist = model.predict(xi)
 
-    # Sampler
-    step = pm.NUTS()
+    # Prior weight distribution
+    ax = prettify_ax(grid[3 * i])
+    ax.set_title(f'Prior weight distribution #{i + 1}')
+    ax.contourf(w, w, model.weights_dist.pdf(W), N, cmap='viridis')
+    ax.scatter(*weights, color='red')  # true weights the model has to find
 
-    # Posterior distribution
-    linear_trace = pm.sample(1000, step)
+    # Update model
+    model.learn(xi, yi)
+
+    # Prior weight distribution
+    ax = prettify_ax(grid[3 * i + 1])
+    ax.set_title(f'Posterior weight distribution #{i + 1}')
+    ax.contourf(w, w, model.weights_dist.pdf(W), N, cmap='viridis')
+    ax.scatter(*weights, color='red')  # true weights the model has to find
+
+    # Posterior target distribution
+    xs.append(xi)
+    ys.append(yi)
+    posteriors = [model.predict(np.array([1, wi])) for wi in w]
+    ax = prettify_ax(grid[3 * i + 2])
+    ax.set_title(f'Posterior target distribution #{i + 1}')
+    # Plot the old points and the new points
+    ax.scatter([xi[1] for xi in xs[:-1]], ys[:-1])
+    ax.scatter(xs[-1][1], ys[-1], marker='*')
+    # Plot the predictive mean along with the predictive interval
+    ax.plot(w, [p.mean() for p in posteriors], linestyle='--')
+    cis = [p.interval(.95) for p in posteriors]
+    ax.fill_between(
+        x=w,
+        y1=[ci[0] for ci in cis],
+        y2=[ci[1] for ci in cis],
+        alpha=.1
+    )
+    # Plot the true target distribution
+    ax.plot(w, [np.dot(weights, [1, xi]) for xi in w], color='red')
 
 
-pm.traceplot(linear_trace, figsize = (12, 12));
-pm.plot_posterior(linear_trace, figsize = (12, 10), text_size = 20);
-pm.forestplot(linear_trace);
 
-plt.figure(figsize = (8, 8));
-pm.plot_posterior_predictive_glm(linear_trace, samples = 100, eval=np.linspace(2, 30, 100), linewidth = 1,color = 'red', alpha = 0.8, label = 'Bayesian Posterior Fits',lm = lambda x, sample: sample['Intercept'] + sample['slope'] * x);
-plt.scatter(X['Duration'], y.values, s = 12, alpha = 0.8, c = 'blue', label = 'Observations');
-plt.plot(X['Duration'], by_hand_coefs[0] + X['Duration'] * by_hand_coefs[1], 'k--', label = 'OLS Fit', linewidth = 1.4);
-plt.title('Posterior Predictions with all Observations', size = 20); plt.xlabel('Duration (min)', size = 18);
-plt.ylabel('Calories', size = 18);
-plt.legend(prop={'size': 16});
-pm.df_summary(linear_trace)
+# A nice property about Bayesian models is that they allow to quantify the uncertainty of predictions.
+np.random.seed(42)
 
-bayes_prediction = linear_trace['Intercept'] + linear_trace['slope'] * 15.5
+model = BayesLinReg(n_features=2, alpha=1, beta=25)
+pct_in_ci = 0
+pct_in_ci_hist = []
+n = 5_000
 
-plt.figure(figsize = (8, 8))
-plt.style.use('fivethirtyeight')
-sns.kdeplot(bayes_prediction, label = 'Bayes Posterior Prediction')
-plt.vlines(x = by_hand_coefs[0] + by_hand_coefs[1] * 15.5, ymin = 0, ymax = 2.5, label = 'OLS Prediction',colors = 'red', linestyles='--')
-plt.legend();
-plt.xlabel('Calories Burned', size = 18), plt.ylabel('Probability Density', size = 18);
-plt.title('Posterior Prediction for 15.5 Minutes', size = 20);
+for i, (xi, yi) in enumerate(sample(n)):
+
+    ci = model.predict(xi).interval(.95)
+    in_ci = ci[0] < yi < ci[1]
+    pct_in_ci += (in_ci - pct_in_ci) / (i + 1)  # online update of an average
+    pct_in_ci_hist.append(pct_in_ci)
+
+    model.learn(xi, yi)
+
+fig, ax = plt.subplots(figsize=(9, 6))
+ax.plot(range(n), pct_in_ci_hist)
+ax.axhline(y=.95, color='red', linestyle='--')
+ax.set_title('Quality of the prediction interval along time')
+ax.set_xlabel('# of observed samples')
+ax.set_ylabel('% of predictions in 95% prediction interval')
+ax.set_ylim(.9, 1)
+ax.grid()
